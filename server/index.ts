ce612fde232a7787c4fdb9b731beac35e4f82586
Manuel Kampert <Manuel.Kampert@bdo-digital.eu>
2026-05-22 19:56:14 +0200

    index t o 8080

--- server/index.ts
+++ server/index.ts
@@ -16,8 +16,7 @@ async function startServer() {
   }
 
   const { app } = await createApp(connectionString);
-  const port = Number(process.env.PORT) || 3000;
-
+const port = Number(process.env.PORT) || 8080;
   app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${port}`);
   });
